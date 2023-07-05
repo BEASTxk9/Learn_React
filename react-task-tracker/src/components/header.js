6c467165f01cdc203dd281a17278f0aca4222493
Shane Stevens <102969628+BEASTxk9@users.noreply.github.com>
2023-07-05 12:25:17 +0200

    update

--- react-task-tracker/src/components/header.js
+++ react-task-tracker/src/components/header.js
@@ -1,11 +1,21 @@
 // if you have the " ES7 React/Redux/GraphQL/React-Native snippets " extension in vsc you can type " rafce " to get a template similar to (vue)Vueinti/!(html)
 // import React from 'react'
 
+// prop types not nessasry but just for learning purposes
+import propTypes from 'prop-types';
+import Button from './Button';
+
+
 const header = ({title}) => {                                          // if you add props in App.js eg. <Header title="hello" /> you can call it inside the function brakets eg. (props) and then call it below in curly brackets eg. <p>{props.title}</p>
-  return (
-    <header>
-        {/* <h1>Task Tracker</h1> */}
-        <p>{title}</p>
+  return (                          
+    // add inline styling eg. <p style={{color: 'red', backgroundColor: 'black'}}>{title}</p> the css attributes are slightly diffrent eg. background-color is backgroundColor in React
+    // add outside styleing eg. <p style={headingStyle}>{title}</p>     
+   <header className="header">
+    <div className="form-heading">
+        <h1>{title}</h1>
+        <Button />
+    </div>
+
     </header>
   )
 }
@@ -16,5 +26,19 @@ const header = ({title}) => {                                          // if you
 header.defaultProps = {
     title: 'Task Tracker',
 }
+
+// call propType to check the data in the variable matches the condition, string,number,...ect
+header.propTypes = {
+    title: propTypes.string.isRequired,                                          // this can be called in from App.js file <Header /> tag eg. <Header title="hello"/> and displayed in header.js in the header function brackets eg. (props) and then in the code <p>props.title</p> 
+    number: propTypes.number.isRequired,                                         // this can be called in from App.js file <Header /> tag eg. <Header number={1}/> and displayed in header.js in the header function brackets eg. (props) and then in the code <p>props.number</p> 
+}
+
+// CSS IN JS(set outside styling)/you can just use index.css
+const headingStyle = {
+color : 'red',
+backgroundColor : 'black', 
+}
+
+
 
 export default header
